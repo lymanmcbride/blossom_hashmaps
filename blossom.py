944473e02ea63e609cb461d5302e300b4cbd03de
Achilles <68021552+lymanmcbride@users.noreply.github.com>
2020-09-30 12:13:31 -0600

    hashmap working off of an array. Following will be linkedlists and nodes

--- blossom.py
+++ blossom.py
@@ -1,11 +1,23 @@
+from linked_list import Node
 class hashmap:
     def __init__(size):
         self.array_size = size
-        self.array = []
+        self.array = [None for i in range(self.array_size)]
 
     def hash(self, key):
         sum(key.encode())
     def compress(hash_code):
         result = hash_code % self.array_size
         return result
-    
+
+    def assign(self, key, value):
+        array_index = self.compress(self.hash(key))
+        self.array[array_index] = [key, value]
+    def retrieve(self, key):
+        array_index = self.compress(self.hash(key))
+        payload = self.array[array_index]
+        if payload:
+            if payload[0] == key:
+                return payload[1]
+            else:
+                return None

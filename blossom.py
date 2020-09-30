1b4e07381635c7bcba96f729977677b8c6199c2c
Achilles <68021552+lymanmcbride@users.noreply.github.com>
2020-09-30 12:25:13 -0600

    changed hashmap to work with nodes and linkedlists

--- blossom.py
+++ blossom.py
@@ -2,7 +2,7 @@ from linked_list import Node
 class hashmap:
     def __init__(size):
         self.array_size = size
-        self.array = [None for i in range(self.array_size)]
+        self.array = [LinkedList() for i in range(self.array_size)]
 
     def hash(self, key):
         sum(key.encode())
@@ -12,12 +12,19 @@ class hashmap:
 
     def assign(self, key, value):
         array_index = self.compress(self.hash(key))
-        self.array[array_index] = [key, value]
+        payload = Node([key, value])
+        list_at_array = self.array[array_index]
+        for item in list_at_array:
+            if item[0] == key:
+                item[1] = value
+        list_at_array.insert(payload)
+
     def retrieve(self, key):
         array_index = self.compress(self.hash(key))
-        payload = self.array[array_index]
-        if payload:
-            if payload[0] == key:
-                return payload[1]
+        list_at_index = self.array[array_index]
+        for item in list_at_index:
+            if item[0] == key:
+                return item[1]
             else:
                 return None
+        

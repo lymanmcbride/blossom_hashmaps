413e0af3aab559a501390d9ab7fd5423edcf63c5
Achilles <68021552+lymanmcbride@users.noreply.github.com>
2020-09-30 12:04:22 -0600

    added constructor and internal hash functions hash and compress

---
+++ blossom.py
@@ -0,0 +1,11 @@
+class hashmap:
+    def __init__(size):
+        self.array_size = size
+        self.array = []
+
+    def hash(self, key):
+        sum(key.encode())
+    def compress(hash_code):
+        result = hash_code % self.array_size
+        return result
+    
